0cd22b0b99c22cf9fafcbdf71c0f6bc266c979c3
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-14 13:59:23 -0500

    Create string_times.py

---
+++ Codingbat_Challenges/Warmup_2/string_times.py
@@ -0,0 +1,14 @@
+"""
+From: https://codingbat.com/prob/p193507
+
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+
+Solution:
+"""
+
+def string_times(str, n):
+    return str * n
