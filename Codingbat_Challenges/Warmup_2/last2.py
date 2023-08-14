3196f49f0692e22972bc596c8b56a4aa7ef3c346
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-14 13:48:56 -0500

    Create last2.py

---
+++ Codingbat_Challenges/Warmup_2/last2.py
@@ -0,0 +1,18 @@
+"""
+From: https://codingbat.com/prob/p145834
+
+Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+last2('hixxhi') → 1
+last2('xaxxaxaxx') → 1
+last2('axxxaaxx') → 2
+
+Solution:
+"""
+
+def last2(str):
+    c = 0
+    for i in range(len(str) - 2):
+      if str[i:i + 2] == str[-2:]:
+        c += 1
+    return c
