b5f6bf63a13ea9fdc6f40ec7c21e515d71cd29a5
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-15 08:14:49 -0500

    Create non_start.py

---
+++ Codingbat_Challenges/String_1/non_start.py
@@ -0,0 +1,14 @@
+"""
+From: https://codingbat.com/prob/p127703
+
+Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+non_start('Hello', 'There') → 'ellohere'
+non_start('java', 'code') → 'avaode'
+non_start('shotl', 'java') → 'hotlava'
+
+Solution:
+"""
+
+def non_start(a, b):
+    return a[1:] + b[1:]
