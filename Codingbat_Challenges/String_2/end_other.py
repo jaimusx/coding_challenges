d9c416f482f7a2e11e0f1002e80345ccb3624ea4
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-15 14:49:30 -0500

    Create end_other.py

---
+++ Codingbat_Challenges/String_2/end_other.py
@@ -0,0 +1,16 @@
+"""
+From: https://codingbat.com/prob/p174314
+
+Given two strings, return True if either of the strings appears at the very end of the other string, 
+ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). 
+Note: s.lower() returns the lowercase version of a string.
+
+end_other('Hiabc', 'abc') → True
+end_other('AbC', 'HiaBc') → True
+end_other('abc', 'abXabc') → True
+
+Solution:
+"""
+
+def end_other(a, b):
+    return a.lower().endswith(b.lower()) or b.lower().endswith(a.lower())
