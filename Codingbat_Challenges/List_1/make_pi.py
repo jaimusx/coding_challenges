c7d41d4cbe8ffc27e19d8f5957ccde7989a7b46c
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-15 08:28:05 -0500

    Create make_pi.py

---
+++ Codingbat_Challenges/List_1/make_pi.py
@@ -0,0 +1,12 @@
+"""
+From: https://codingbat.com/prob/p113659
+
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+make_pi() → [3, 1, 4]
+
+Solution:
+"""
+
+def make_pi():
+    return [3, 1, 4]
