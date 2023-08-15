e4f329a57e02a048cb2bebd069c38da45d13e7a3
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-15 07:24:36 -0500

    Create front_back.py

---
+++ Codingbat_Challenges/Warmup_1/front_back.py
@@ -0,0 +1,14 @@
+"""
+From: https://codingbat.com/prob/p153599
+
+Given a string, return a new string where the first and last chars have been exchanged.
+
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+
+Solution:
+"""
+
+def front_back(str):
+    return str if len(str) <= 1 else str[-1] + str[1:-1] + str[0]
