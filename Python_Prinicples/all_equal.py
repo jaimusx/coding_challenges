2cfa7ddba6ced0339d3f0833b5a4728e37c9bdc9
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-16 14:21:49 -0500

    Create all_equal.py

---
+++ Python_Prinicples/all_equal.py
@@ -0,0 +1,10 @@
+"""
+Define a function named all_equal that takes a list and checks whether all elements in the list are the same.
+
+For example, calling all_equal([1, 1, 1]) should return True.
+
+Solution:
+"""
+
+def all_equal(arr):
+    return arr == arr[::-1]
