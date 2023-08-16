34917b97b359342c2fe2b3de038877b5b636afc1
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-16 14:26:17 -0500

    Create boolean_and.py

---
+++ Python_Prinicples/boolean_and.py
@@ -0,0 +1,8 @@
+"""
+Define a function named triple_and that takes three parameters and returns True only if they are all True and False otherwise.
+
+Solution:
+"""
+
+def triple_and(a, b, c):
+    return a and b and c
