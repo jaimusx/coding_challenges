37ce164b75e215e8ba0982768c5560f427809879
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-16 07:22:37 -0500

    Update squirrel_play.py

--- Codingbat_Challenges/Logic_1/squirrel_play.py
+++ Codingbat_Challenges/Logic_1/squirrel_play.py
@@ -1,7 +1,9 @@
 """
 From: https://codingbat.com/prob/p135815
 
-The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, return True if the squirrels play and False otherwise.
+The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). 
+Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, 
+return True if the squirrels play and False otherwise.
 
 squirrel_play(70, False) → True
 squirrel_play(95, False) → False
