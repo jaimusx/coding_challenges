42a41f927c5709f7a5b89fe72f829ba9ea29d359
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-16 07:33:45 -0500

    Update missing_char.py

--- Codingbat_Challenges/Warmup_1/missing_char.py
+++ Codingbat_Challenges/Warmup_1/missing_char.py
@@ -1,7 +1,8 @@
 """
 From: https://codingbat.com/prob/p149524
 
-Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. 
+The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
 
 missing_char('kitten', 1) → 'ktten'
 missing_char('kitten', 0) → 'itten'
