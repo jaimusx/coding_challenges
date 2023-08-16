c8a47fb3ea45b76557d6db1362e42f387eebe121
Jaime Aviles <62559923+jaimusx@users.noreply.github.com>
2023-08-16 07:31:12 -0500

    Update combo_string.py

--- Codingbat_Challenges/String_1/combo_string.py
+++ Codingbat_Challenges/String_1/combo_string.py
@@ -1,7 +1,8 @@
 """
 From: https://codingbat.com/prob/p194053
 
-Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. 
+The strings will not be the same length, but they may be empty (length 0).
 
 combo_string('Hello', 'hi') → 'hiHellohi'
 combo_string('hi', 'Hello') → 'hiHellohi'
